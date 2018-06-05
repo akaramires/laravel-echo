439c5fdeb3134421ea4d54aa549f1f18efd4d8c0
Elmar Abdurayimov <e.abdurayimov@gmail.com>
2018-06-05 22:38:22 +0300

    Episode 1

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/', function () {
+    \App\Events\OrderStatusUpdated::dispatch();
+
+    return view( 'welcome' );
+} );
